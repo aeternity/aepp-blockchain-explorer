959762b0d659535eec0a499a302c1c0ada22e2bb
Philipp Klein <klein.philipp@gmail.com>
2018-02-14 17:22:05 +0100

    address screen with account related aepp-components

--- src/screens/address/address.vue
+++ src/screens/address/address.vue
@@ -1,29 +1,43 @@
 <template>
   <div class='account-screen screen'>
-    <h1 class='title'>Account</h1>
+    <h1 class='title'>
+      <ae-identity-avatar :address="address"/>
+      <ae-address size='compact' :show-avatar='false' :address='address'/>
+    </h1>
+
+    <div class="field">
+      <div class='field-name'>Balance</div>
+      <div>
+        <span class="number">{{balance}}</span>
+        <span class="unit">AE</span>
+      </div>
+    </div>
+
     <div class="field">
       <div class="field-name">
         Public Key
       </div>
-      <div clas='account-address'>{{address}}</div>
+      <div class='account-address'>
+        <ae-address :show-avatar='false' :address='address'/>
+      </div>
     </div>
 
-    <div class="field">
-    <div class='field-name'>Balance</div>
-    <div>
-      <span class="number">{{balance}}</span>
-      <span class="unit">AE</span>
-    </div>
-
-    </div>
     <!--<h2>List of Transactions:</h2>-->
   </div>
 </template>
 <script>
 // ak$3MUyXbU7Dzf6Urgn4wnKocxG5LH4rRrrMrXXPXB9sh7BkrRhiupwxLW92zmj2KtLvABzBb76LQtQKAdos7sFyX4do3EKto
 // const addressRegex = RegExp('^ak\\$[1-9A-HJ-NP-Za-km-z]{94}')
+import {
+  AeAddress,
+  AeIdentityAvatar
+} from '@aeternity/aepp-components'
 export default {
   name: 'Address',
+  components: {
+    AeAddress,
+    AeIdentityAvatar
+  },
   data () {
     return {
       balance: null
