9790957583354f890fb1df0419adf5e45aa4cc6c
Philipp Klein <klein.philipp@gmail.com>
2018-02-14 22:47:46 +0100

    market stats better display

--- src/partials/marketStats/marketStats.vue
+++ src/partials/marketStats/marketStats.vue
@@ -4,26 +4,29 @@
       <div class="market-cap">
         <div>Market Cap</div>
         <span v-if='apiMarket' class="number">
-          {{apiMarket.market_cap_chf}}
+          {{apiMarket.market_cap_chf / 1000000 | round}}
+          M
         </span>
         <span class="unit">CHF</span>
       </div>
       <div class="div btc-price">
         <div>1 AE</div>
         <span v-if='apiMarket' class="number">
-          {{apiMarket.price_btc}}
+          {{apiMarket.price_btc | round(6) }}
         </span>
         <span class="unit">BTC</span>
       </div>
       <div class="chf-price">
         <div>1 AE</div>
         <span v-if='apiMarket' class="number">
-          {{apiMarket.price_chf}}
+          {{apiMarket.price_chf | round }}
         </span>
         <span class="unit">CHF</span>
       </div>
     </div>
-    <!--<div class="button">View all stats</div>-->
+    <div class="source">
+      data provided by: <a href="https://coinmarketcap.com/currencies/aeternity" target="_blank">coinmarketcap.com</a>
+    </div>
   </div>
 </template>
 <script>
