d4f37a76b772ed970b40b45b9ce2339d2af69a97
Philipp Klein <klein.philipp@gmail.com>
2018-04-24 16:13:47 +0200

    added field component

---
+++ src/components/field/field.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="field">
+    <div class="field-name">{{name}}</div>
+    <div class="field-value">
+      <slot/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'field',
+  props: ['name']
+}
+</script>
+<style scoped lang='scss'>
+.field {
+  display:flex;
+  justify-content:space-between;
+  padding:8px 0;
+}
+.field-name {
+  @include font-size(s);
+  color:$black;
+  opacity:0.7;
+}
+.field-value {
+  @include font-size(m);
+  font-family:'Roboto Mono', monospace;
+  align-items:center;
+  font-weight:500;
+  display:flex;
+  vertical-align:middle;
+}
+.field-value > a {
+  color:$magaenta;
+}
+</style>
