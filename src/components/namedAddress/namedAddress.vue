be907bfd32d1cfdcaa81be7d4f915e3a0486cb0d
Philipp Klein <klein.philipp@gmail.com>
2018-04-25 19:57:18 +0200

    Add NamedAddress component

---
+++ src/components/namedAddress/namedAddress.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="named-address">
+    <template v-if='address'>
+      <template v-if='name'>
+        {{name}}
+      </template>
+      <ae-address v-else size='compact' :show-avatar='false' :address='address'/>
+    </template>
+  </div>
+</template>
+<script>
+import {
+  mapState
+} from 'vuex'
+import {
+  AeAddress,
+  AeIdentityAvatar
+} from '@aeternity/aepp-components'
+export default {
+  name: 'named-address',
+  props: ['address'],
+  components: {
+    AeAddress,
+    AeIdentityAvatar
+  },
+  computed: {
+    ...mapState({
+      name (state) {
+        return state.accountNames[this.address]
+      }
+    })
+  }
+}
+</script>
+<style>
+</style>
