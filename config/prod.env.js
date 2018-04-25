5a1d5602edb693a8f3d248ff0523874e58ce83d6
Philipp Klein <klein.philipp@gmail.com>
2018-04-25 17:33:29 +0200

    NAME_LOOKUP_MIDDLEWARE_URL env var fixed

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,5 @@ module.exports = {
   AETERNITY_EPOCH_API_URL : `"${process.env.AETERNITY_EPOCH_API_URL || '//test-net-api.aepps.com/api/'}"`,
   SHOW_MARKET_STATS: process.env.SHOW_MARKET_STATS || true,
   SHOW_NETWORK_STATS: process.env.SHOW_NETWORK_STATS || true,
-  NAME_LOOKUP_MIDDLEWARE_URL: process.env.NAME_LOOKUP_MIDDLEWARE_URL || false
-
+  NAME_LOOKUP_MIDDLEWARE_URL: process.env.NAME_LOOKUP_MIDDLEWARE_URL ? `"${process.env.NAME_LOOKUP_MIDDLEWARE_URL}"` : false
 }
