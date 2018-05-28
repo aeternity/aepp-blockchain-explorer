e50a77375cf9a28d3cfb26a1d570f9d75708bf53
SamLife91 <sbarguti@quanterall.com>
2018-05-28 18:08:44 +0300

    add mobile navigation  - add svg icons  - style mobile nav  - build mobile nav layout and funct.

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,98 @@
     <nav>
       <div class="inner">
         <router-link class='logo' to='/'>
-          <img src="@/assets/header-logo.svg" alt=""/>
-          <span>explorer</span>
+          <img src="@/assets/header-logo.svg" alt="" />
+          <span class="app-name">explorer</span>
         </router-link>
         <div class="links">
           <router-link class='link' to='/'>
             Dashboard
           </router-link>
-          <router-link class='link'  to='/blocks'>
+          <router-link class='link' to='/blocks'>
             Blocks
           </router-link>
           <router-link class='link' to='/tx'>
             Transactions
           </router-link>
         </div>
+        <div class="mobile-nav">
+          <div class="pageName">
+            <strong v-if="$route.name === 'Index'">Dashboard</strong>
+            <strong v-if="$route.name === 'BlockList'">Blocks</strong>
+            <strong v-if="$route.name === 'Transactions'">Transactions</strong>
+          </div>
+          <div class="burger" @click='expandMenu'>
+            <img src="@/assets/burger.svg" alt="" />
+          </div>
+          <div class="mobile-nav-fixed" :class="{ active: isOpened}">
+            <div class="mobile-nav-fixed-holder">
+              <div class="inner">
+                <div class="mobile-nav-fixed-action">
+                  <span @click='expandMenu'>
+                    <img src="@/assets/close.svg" alt="" />
+                  </span>
+                </div>
+                <div class="mobile-nav-fixed-main-links">
+                  <router-link to='/'>
+                    Dashboard
+                  </router-link>
+                  <router-link to='/blocks'>
+                    Blocks
+                  </router-link>
+                  <router-link to='/tx'>
+                    Transactions
+                  </router-link>
+                </div>
+                <div class="mobile-nav-fixed-secondary-links">
+                  <router-link to='/'>
+                    imprint
+                  </router-link>
+                  <router-link to='/'>
+                    get started
+                  </router-link>
+                  <router-link to='/'>
+                    aeternity.com
+                  </router-link>
+                </div>
+                <div class="mobile-nav-fixed-socials-links">
+                  <div class="mobile-nav-fixed-socials-links-row">
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                  </div>
+                  <div class="mobile-nav-fixed-socials-links-row">
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                    <router-link to='/'>
+                      <img src="@/assets/github.svg" alt="">
+                    </router-link>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
         <div>
-            <span v-if='env.SHOW_NETWORK_STATS' is='router-link' to='/status/' class='network-name'>{{networkName}}</span>
+          <span v-if='env.SHOW_NETWORK_STATS' is='router-link' to='/status/' class='network-name'>{{networkName}}</span>
           <!--<router-link to='/search'>-->
-            <!--<img src="@/assets/search.svg" alt=""/>-->
+          <!--<img src="@/assets/search.svg" alt=""/>-->
           <!--</router-link>-->
         </div>
       </div>
@@ -32,6 +106,8 @@
   </div>
 </template>
 
+
+
 <script>
 import Footer from '@/partials/footer/footer.vue'
 import {mapState} from 'vuex'
@@ -39,6 +115,11 @@ export default {
   name: 'app',
   components: {
     AeFooter: Footer
+  },
+  data () {
+    return {
+      isOpened: false
+    }
   },
   computed: {
     ...mapState(['env']),
@@ -54,8 +135,23 @@ export default {
       }
       return url
     }
+  },
+  methods: {
+    expandMenu: function () {
+      console.log('in')
+      this.isOpened = !this.isOpened
+    }
   }
 }
 </script>
 
 <style src='./App.scss' lang='scss' />
+<style lang='scss'>
+  svg {
+    fill:white;
+  }
+  img svg{
+    fill:white !important;
+  }
+</style>
+
