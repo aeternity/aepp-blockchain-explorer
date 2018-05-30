60b59a7e38ca5f1cd542db47829fb89094f79d50
SamLife91 <sbarguti@quanterall.com>
2018-05-30 18:24:43 +0300

    fixed footer social icons layouts on mobile

--- src/partials/footer/footer.vue
+++ src/partials/footer/footer.vue
@@ -12,7 +12,8 @@
         <a href="https://www.aepps.com/imprint/" target="_blank">Imprint</a>
         <a href="https://www.aepps.com/get-started-with-ae/" target="_blank">Get started</a>
       </div>
-      <div>
+      <div class="footer-social-links">
+        <div>
         <a href="https://github.com/aeternity" target="_blank"><img src="@/assets/github.svg" alt=""/>Github</a>
         <a href="https://www.reddit.com/r/Aeternity/" target="_blank"><img src="@/assets/reddit.svg" alt=""/>Reddit</a>
         <a href="https://blog.aeternity.com/" target="_blank"><img src="@/assets/medium.svg" alt=""/>Medium</a>
@@ -23,6 +24,7 @@
         <a href="https://www.facebook.com/aeternityproject/" target="_blank"><img src="@/assets/facebook.svg" alt=""/>Facebook</a>
         <a href="https://twitter.com/aetrnty" target="_blank"><img src="@/assets/twitter.svg" alt=""/>Twitter</a>
         <a href="https://www.linkedin.com/company/aeternity" target="_blank"><img src="@/assets/linkedin.svg" alt=""/>Linkedin</a>
+      </div>
       </div>
     </div>
   </footer>
