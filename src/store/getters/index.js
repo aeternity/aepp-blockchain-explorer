e3a6bd4ec9127bfa8e26e18897014bfca5696f06
sqevani <ina.veq@gmail.com>
2018-06-11 15:22:11 +0900

    overall refactoring of the store

--- src/store/getters/index.js
+++ src/store/getters/index.js
@@ -1,13 +1,4 @@
 /**
  * Exporting Getters
  */
-export default {
-  /**
-   * Template
-   * @param {{}} state
-   * @return {Function}
-   */
-  template: function (state) {
-    return state
-  }
-}
+export default {}
