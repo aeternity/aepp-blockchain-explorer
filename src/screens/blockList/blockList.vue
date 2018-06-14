ce7578c6bedd3ea27982dfeb04c52361ddd8e924
sqevani <ina.veq@gmail.com>
2018-06-14 14:40:42 +0900

    cleaning up blocklist and making it work

--- src/screens/blockList/blockList.vue
+++ src/screens/blockList/blockList.vue
@@ -1,135 +1,123 @@
 <template>
   <div class="block-list-screen screen">
     <div class="inner">
-    <div class="grid">
-      <div class='title'>
-        Blocks
+      <div class="grid">
+        <div class="title">Blocks</div>
+        <div class="field average-block-time">
+          <span class="field-name">average rate 1 per </span>
+          <relative-time :ts="getAverageBlockTime" big spaced/>
+        </div>
+        <div>
+          <span class="field-name">last block mined</span>
+          <relative-time :ts="getLastMinedBlockTime(this.currentTime)" big spaced/>
+          <span class="field-name">ago</span>
+        </div>
       </div>
-      <div class="field average-block-time">
-        <span class='field-name'>average rate 1 per </span>
-        <relative-time :ts="averageBlockTime" big spaced />
-      </div>
-      <div>
-        <span class="field-name">
-        last block mined
-        </span>
-        <relative-time :ts="lastBlockAgo" big spaced />
-        <span class="field-name">
-        ago
-        </span>
-     </div>
-    </div>
 
-    <table class='transactions'>
-      <tr v-for='b in blocks'>
-        <template v-if="b">
-          <td>
-            <div class="block-number">
-              <router-link :to='"/block/" + b.height'>
-                {{b.height}}
-              </router-link>
-            </div>
-          </td>
-          <td>
-            <span class="field-name">
-              hash
-            </span>
-            <span v-if='b.hash' class="number">
-              <ae-hash type='short' :hash='b.hash'/>
-            </span>
-            <span v-else>n/a</span>
-          </td>
-          <td>
-            <span class="field-name">
-            target
-            </span>
-            <span class="number">
-              {{b.target}}
-            </span>
-          </td>
-          <td>
-            <span class='number'>
-              {{b.transactions.length}}
-            </span>
-            <span class="field-name">
-              Transaction(s)
-            </span>
-          </td>
-          <td>
-            <span class="field-name">
-              mined by
-            </span>
-            <span class="account-address">
-              <router-link :to='"/account/" + b.miner'>
-                <named-address :address='b.miner'/>
-              </router-link>
-            </span>
-          </td>
-          <td>
-            <span class="field-name">
-              time
-            </span>
-            <span class="number">
-              <relative-time :ts="currentTime - b.time" />
-            </span>
-          </td>
-        </template>
-        <template v-else>
-          <td colspan="3">
-          Loading..
-          </td>
-        </template>
-      </tr>
-    </table>
-    <div class="center">
-      <ae-button invert type='exciting' @click='loadMore'>
-        load more
-      </ae-button>
-    </div>
+      <table class="transactions">
+        <tr v-for="b in blocks">
+          <template v-if="b">
+            <td>
+              <div class="block-number">
+                <router-link :to="`/block/${b.height}`">{{ b.height }}</router-link>
+              </div>
+            </td>
+            <td>
+              <span class="field-name">
+                hash
+              </span>
+              <span v-if="b.hash" class="number">
+                <ae-hash type="short" :hash="b.hash"/>
+              </span>
+              <span v-else>n/a</span>
+            </td>
+            <td>
+              <span class="field-name">target</span>
+              <span class="number">{{b.target}}</span>
+            </td>
+            <td>
+              <span class="number">{{ b.transactions.length }}</span>
+              <span class="field-name">Transaction(s)</span>
+            </td>
+            <td>
+              <span class="field-name">mined by</span>
+              <span class="account-address">
+                <router-link :to="`/account/${b.miner}`">
+                  <named-address :address="b.miner"/>
+                </router-link>
+              </span>
+            </td>
+            <td>
+              <span class="field-name">time</span>
+              <span class="number">
+                <relative-time :ts="currentTime - b.time"/>
+              </span>
+            </td>
+          </template>
+          <template v-else>
+            <td colspan="3">Loading..</td>
+          </template>
+        </tr>
+      </table>
+      <div class="center">
+        <ae-button invert type="exciting" @click="loadMore">load more</ae-button>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-import {
-  AeButton
-} from '@aeternity/aepp-components'
-import currentTime from '../../mixins/currentTime'
-import pollAction from '../../mixins/pollAction'
+import { mapState, mapGetters } from 'vuex';
+import { AeButton } from '@aeternity/aepp-components';
+import currentTime from '../../mixins/currentTime';
+import pollAction from '../../mixins/pollAction';
+
 export default {
+  /*
+   * Screen Components
+   */
   components: { AeButton },
-  mixins: [currentTime, pollAction('loadLastBlocks', [10])],
+
+  /*
+   * Screen Mixins
+   */
+  mixins: [
+    currentTime,
+    // Currently this looks more like a hack
+    // would be good to improve the way we poll
+    // data and refresh the page
+    pollAction('blocks/getLatestBlocks', [10])
+  ],
+
+  /*
+   * Computed Properties
+   */
   computed: {
-    ...mapState({
-      blocks: state => {
-        const blocks = []
-        let height = Math.max(...Object.keys(state.blocks).filter(i => !isNaN(+i)))
-        while (state.blocks[height]) {
-          blocks.push(state.blocks[height])
-          height--
-        }
-        return blocks
-      }
-    }),
-    averageBlockTime () {
-      const blockTimes = this.blocks.map((block, idx, blocks) =>
-        idx + 1 < blocks.length && blocks[idx].time - blocks[idx + 1].time)
-      blockTimes.pop()
-      return blockTimes.reduce((a, b) => a + b, 0) / blockTimes.length
-    },
-    lastBlockAgo () {
-      return this.blocks[0] ? this.currentTime - this.blocks[0].time : null
-    }
+    /*
+     * map blocks unto computed
+     * properties
+     */
+    ...mapState('blocks', [
+      'blocks'
+    ]),
+
+    /*
+     * map block getters into
+     * the computed properties
+     */
+    ...mapGetters('blocks', [
+      'getAverageBlockTime',
+      'getLastMinedBlockTime'
+    ])
   },
+
+  /*
+   * Screen methods
+   */
   methods: {
-    loadMore () {
-      for (let i = 1; i <= 10; i++) {
-        this.$store.dispatch('loadBlock', {
-          height: this.blocks[this.blocks.length - 1].height - i
-        })
-      }
+    loadMore: function () {
+      return this.$store.dispatch('blocks/getLatestBlocks', this.blocks.length + 10);
     }
   }
 }
 </script>
-<style src='./blockList.scss' lang='scss' />
+<style src='./blockList.scss' lang='scss'/>
