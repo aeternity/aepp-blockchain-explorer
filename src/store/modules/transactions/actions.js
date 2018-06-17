7f9de58a9fd5f75df5a258e01b48be38a9c0f3d2
sqevani <ina.veq@gmail.com>
2018-06-17 16:38:21 +0900

    transactions vuex-store module and some updates

--- src/store/modules/transactions/actions.js
+++ src/store/modules/transactions/actions.js
@@ -1,6 +1,7 @@
 /**
  * Importing Libraries
  */
+import ae from '../../aeppsdk'
 import { createActionHelpers } from 'vuex-loading'
 
 /**
@@ -15,7 +16,7 @@ const { startLoading, endLoading } = createActionHelpers({
  */
 export default {
   /**
-   * get the transactions of a specific hash
+   * get the transactions in the mempool
    * @param {Object} state
    * @param {Function} commit
    * @param {Function} dispatch
@@ -25,8 +26,81 @@ export default {
   async get ({ state, commit, dispatch }, hash) {
     startLoading(dispatch, 'transactions/get')
 
-    commit('setTransactions', [])
+    const client = await ae
+    const transactions = await client.api.getTxs()
 
-    return endLoading(dispatch, 'transactions/get')
+    commit('setTransactions', transactions)
+
+    endLoading(dispatch, 'transactions/get')
+
+    return transactions
+  },
+
+  /**
+   * get transactions in the mempool
+   * @param {Object} state
+   * @param {Function} commit
+   * @param {Function} dispatch
+   * @param {String} hash
+   * @return {*}
+   */
+  async mempool ({ state, commit, dispatch }, hash) {
+    startLoading(dispatch, 'transactions/mempool')
+
+    const client = await ae
+    const mempoolTxs = await client.api.getTxs()
+
+    commit('setMempoolTxs', mempoolTxs)
+
+    endLoading(dispatch, 'transactions/mempool')
+
+    return mempoolTxs
+  },
+
+  /**
+   *
+   * @param state
+   * @param commit
+   * @param dispatch
+   * @param hash
+   * @return {Promise<*>}
+   */
+  async getTransactionsFromBlockHash ({ state, commit, dispatch }, hash) {
+    startLoading(dispatch, 'transactions/getTransactionsFromBlockHash')
+
+    const client = await ae
+    const transactions = await client.api.getTxs()
+
+    commit('setTransactions', transactions)
+
+    endLoading(dispatch, 'transactions/getTransactionsFromBlockHash')
+
+    return transactions
+  },
+
+  /**
+   *
+   * @param state
+   * @param commit
+   * @param dispatch
+   * @param from
+   * @param to
+   * @return {Promise<*>}
+   */
+  async getTxsFromBlocksByHeightRange ({ state, commit, dispatch }, { from, to }) {
+    startLoading(dispatch, 'transactions/getTxsFromBlocksByHeightRange')
+
+    const client = await ae
+    const transactions = await client.api.getTxsListFromBlockRangeByHeight({
+      from,
+      to,
+      txEncoding: 'json'
+    })
+
+    commit('setTransactions', transactions)
+
+    endLoading(dispatch, 'transactions/getTxsFromBlocksByHeightRange')
+
+    return transactions
   }
 }
