326a31be5434e1b1a42d2fe3d7ca31c83e22d65a
Philipp Klein <klein.philipp@gmail.com>
2018-06-26 15:41:10 +0200

    display contract call tx in block screen tx list

--- src/components/transaction/transaction.vue
+++ src/components/transaction/transaction.vue
@@ -227,6 +227,34 @@
         </div>
       </template>
 
+      <template v-else-if='transaction.tx.type === "contract_call_tx"'>
+        <div class="grid">
+          <field name="contract">
+            <router-link :to='"/account/" + transaction.tx.contract'>
+              <named-address :address='transaction.tx.contract'/>
+            </router-link>
+          </field>
+          <field name="caller">
+            <router-link :to='"/account/" + transaction.tx.caller'>
+              <named-address :address='transaction.tx.caller'/>
+            </router-link>
+          </field>
+          <field name='amount'>
+            <span class='number'>{{ transaction.tx.amount }}</span>
+            <span class="unit">AE</span>
+          </field>
+          <field name="gas">
+            {{transaction.tx.gas}}
+          </field>
+          <field name="gasPrice">
+            {{transaction.tx.gasPrice}}
+          </field>
+          <field name="ttl">
+            {{transaction.tx.ttl}}
+          </field>
+        </div>
+      </template>
+
       <template v-else>
         {{transaction}}
       </template>
