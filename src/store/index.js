7f35aea9ce094a06747f0fffcffa03f0bd8fe393
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-08-25 10:57:49 +0200

    store: Simplify root structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import state from './state'
+import { createActionHelpers } from 'vuex-loading'
 import modules from './modules'
-import mutations from './mutations'
-import actions from './actions'
+import ae from './aeppsdk'
 
 Vue.use(Vuex)
 
+/**
+ * Setting up start/end Loading helper methods
+ */
+const { startLoading, endLoading } = createActionHelpers({
+  moduleName: 'loading'
+})
+
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
-  state,
-  mutations,
-  actions,
+
+  state: {
+    $environment: process.env.NODE_ENV,
+    $version: '0.0.0',
+    $platform: 'Blockchain Explorer',
+    $language: 'en',
+    $nodeStatus: {},
+    env: process.env
+  },
+
+  mutations: {
+    /**
+     * setNodeStatus
+     * @param {Object} state
+     * @param $nodeStatus
+     */
+    setNodeStatus (state, $nodeStatus) {
+      return Object.assign(state, { $nodeStatus })
+    }
+  },
+
+  actions: {
+    /**
+     * getNodeStatus
+     * @param {Object} state
+     * @param {Function} commit
+     * @param {dispatch} dispatch
+     * @return {Object}
+     */
+    async getNodeStatus ({ state, commit, dispatch }) {
+      startLoading(dispatch, 'getNodeStatus')
+
+      const client = await ae
+
+      const [top, version] = await Promise.all([
+        client.api.getTop(),
+        client.api.getVersion()
+      ])
+
+      commit('setNodeStatus', { top, version })
+
+      endLoading(dispatch, 'getNodeStatus')
+
+      return { top, version }
+    }
+  },
+
   modules
 })
