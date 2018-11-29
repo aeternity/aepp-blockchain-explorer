d644856eb9d62e6338178071d5e5b66f8a5d00bf
ror-shubham <ror.shubh@gmail.com>
2018-11-29 18:14:44 +0530

    used toString instead of toNumber

--- src/mixins/yaniToAe.js
+++ src/mixins/yaniToAe.js
@@ -3,7 +3,7 @@ import {BigNumber} from 'bignumber.js'
 export default {
   methods: {
     yaniToAe (yani) {
-      return new BigNumber(yani).shiftedBy(-18).toNumber()
+      return new BigNumber(yani).shiftedBy(-18).toString()
     }
   }
 }
