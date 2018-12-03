f95fcb51c78f929e7dff36a859a9cefe746680e9
Denis Davidyuk <denis_davidyuk@hotmail.com>
2018-12-03 09:28:39 +0100

    networkSwitcher: Remove useless wrapper

--- src/components/networkSwitcher.vue
+++ src/components/networkSwitcher.vue
@@ -1,47 +1,45 @@
 <template>
-  <div class="contract-code">
-    <ae-card class="network-switcher">
-      <ae-list face="primary" v-if="isDisplaying ">
-        <div class="network-item"
-             v-for="(network, index) in collectedNetworks" :key="index" :class="{localnetwork: network.isLocal}">
-          <ae-list-item >
-            <ae-dropdown direction="left" v-if="network.isLocal">
-              <ae-icon name="more" size="20px" slot="button" />
-              <li>
-                <ae-icon name="copy" />
-                Copy Link
-              </li>
-              <li>
-                <ae-button>
-                  <ae-icon name="delete" />
-                  Delete
-                </ae-button>
-              </li>
-            </ae-dropdown>
-            <div class="network-info">
-              <div class="network-info__name">
-                {{network.name}}
-              </div>
-              <div class="network-info__url">
-                {{network.url}}
-              </div>
-            </div><ae-check class="network-switcher__action"
-                            v-model="networkUrl"
-                            :value="network.url"
-                            type="radio"
-                            @change="catchNetworkName(network.name, network.url)"/>
-          </ae-list-item>
-        </div>
-      </ae-list>
-      <div class="hendler-wrapper">
-        <error-item class="error" :name="networkName" :onTryAgain="showNetwork"  :onCancel="closeNetwork"  v-if="connectError"></error-item>
-        <loader-item :name="networkName"  v-if="isLoading" ></loader-item>
+  <ae-card class="network-switcher">
+    <ae-list face="primary" v-if="isDisplaying ">
+      <div class="network-item"
+           v-for="(network, index) in collectedNetworks" :key="index" :class="{localnetwork: network.isLocal}">
+        <ae-list-item >
+          <ae-dropdown direction="left" v-if="network.isLocal">
+            <ae-icon name="more" size="20px" slot="button" />
+            <li>
+              <ae-icon name="copy" />
+              Copy Link
+            </li>
+            <li>
+              <ae-button>
+                <ae-icon name="delete" />
+                Delete
+              </ae-button>
+            </li>
+          </ae-dropdown>
+          <div class="network-info">
+            <div class="network-info__name">
+              {{network.name}}
+            </div>
+            <div class="network-info__url">
+              {{network.url}}
+            </div>
+          </div><ae-check class="network-switcher__action"
+                          v-model="networkUrl"
+                          :value="network.url"
+                          type="radio"
+                          @change="catchNetworkName(network.name, network.url)"/>
+        </ae-list-item>
       </div>
-      <ae-toolbar slot="footer"  v-if="isDisplaying ">
-        <ae-button class="network-info__connect" face="flat" @click="showForm()">Connect to other network</ae-button>
-      </ae-toolbar>
-    </ae-card>
-  </div>
+    </ae-list>
+    <div class="hendler-wrapper">
+      <error-item class="error" :name="networkName" :onTryAgain="showNetwork"  :onCancel="closeNetwork"  v-if="connectError"></error-item>
+      <loader-item :name="networkName"  v-if="isLoading" ></loader-item>
+    </div>
+    <ae-toolbar slot="footer"  v-if="isDisplaying ">
+      <ae-button class="network-info__connect" face="flat" @click="showForm()">Connect to other network</ae-button>
+    </ae-toolbar>
+  </ae-card>
 </template>
 
 <script>
