b57cc432bc71c966be474f83ff99722ceff94286
Philipp Klein <klein.philipp@gmail.com>
2018-12-05 12:37:43 +0100

    fix name pre claim tx

--- src/components/transaction/namePreclaimTx.vue
+++ src/components/transaction/namePreclaimTx.vue
@@ -2,12 +2,12 @@
   <div>
     <div class="grid">
       <field name="account">
-        <router-link :to='"/account/" + transaction.tx.account'>
-          <named-address :address='transaction.tx.account'/>
+        <router-link :to='"/account/" + transaction.tx.accountId'>
+          <named-address :address='transaction.tx.accountId'/>
         </router-link>
       </field>
       <field name="commitment">
-        <ae-hash type='short' :hash="transaction.tx.commitment" />
+        <ae-hash type='short' :hash="transaction.tx.commitmentId" />
       </field>
       <field name="fee">
         {{transaction.tx.fee}}
--- src/screens/transactionDetail/namePreclaimTx.vue
+++ src/screens/transactionDetail/namePreclaimTx.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-    <field v-if='transaction.tx.commitment' name="Commitment">
-      <ae-hash type='short' :hash="transaction.tx.commitment" />
-      <view-and-copy :text='transaction.tx.commitment'/>
+    <field name="account">
+      <router-link :to='"/account/" + transaction.tx.accountId'>
+        <named-address :address='transaction.tx.accountId'/>
+      </router-link>
+    </field>
+    <field v-if='transaction.tx.commitmentId' name="Commitment">
+      <ae-hash type='short' :hash="transaction.tx.commitmentId" />
+      <view-and-copy :text='transaction.tx.commitmentId'/>
     </field>
   </div>
 </template>
 <script>
 import Field from '../../components/field'
 import AeHash from '../../components/aeHash'
+import NamedAddress from '../../components/namedAddress'
 import ViewAndCopy from '../../components/viewAndCopy'
 
 export default {
   name: 'name-preclaim-tx',
-  components: { Field, AeHash, ViewAndCopy },
+  components: { Field, AeHash, NamedAddress, ViewAndCopy },
   props: ['transaction']
 }
 </script>
