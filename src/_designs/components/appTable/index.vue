a982d51ddce0674f903f95e7fae2f880092934e7
Emil Sommerfeldt <emil@jungfeldt.de>
2019-01-08 11:58:50 +0100

    combine components to simplify code

--- src/_designs/components/appTable/index.vue
+++ src/_designs/components/appTable/index.vue
@@ -20,16 +20,8 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  @import "~@aeternity/aepp-components-3/src/styles/variables/colors";
-
   .app-table{
     display: flex;
     flex-direction: column
-  }
-  .app-table.details {
-    background-color: $color-neutral-positive-3;
-    @media  (min-width: 450px) {
-      background-color: #FFFFFF;
-    }
   }
 </style>
