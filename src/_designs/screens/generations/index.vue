0e3ac990e20e16c914770328ccf61e7f9a63045c
Emil Sommerfeldt <emil@jungfeldt.de>
2019-01-09 11:18:56 +0100

    add screens and routes for MVP

---
+++ src/_designs/screens/generations/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-generation">
+    <h1>App-Generations</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppGenerations'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/_designs/screens/transactions/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-transactions">
+    <h1>App-Transactions</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTransactions'
+}
+</script>
+
+<style scoped>
+
+</style>
