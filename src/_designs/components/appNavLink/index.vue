30fb152d4be3ce24a028fd8054bcd1a13a361ab5
emilsomm <emil@jungfeldt.de>
2019-01-09 12:49:31 +0100

    add basic nav component (#364)
    * add basic nav component
    
    * add links to other screens

---
+++ src/_designs/components/appNavLink/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <RouterLink
+    :to="to"
+    class="app-nav-link"
+    active-class="active"
+    exact
+  >
+    <slot />
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: 'AppNavLink',
+  props: {
+    to: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/_designs/screens/index.vue
+++ src/_designs/screens/index.vue
@@ -3,12 +3,30 @@
     id="blockchain"
     class="app"
   >
+    <AppNav>
+      <AppNavLink to="dashboard">
+        Dashboard
+      </AppNavLink>
+      <AppNavLink to="generations">
+        Generations
+      </AppNavLink>
+      <AppNavLink to="transactions">
+        Transactions
+      </AppNavLink>
+    </AppNav>
     <RouterView />
   </div>
 </template>
 <script>
+import AppNav from '@/_designs/components/appNav'
+import AppNavLink from '@/_designs/components/appNavLink'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppNav,
+    AppNavLink
+  }
 }
 </script>
 <style lang="scss">
