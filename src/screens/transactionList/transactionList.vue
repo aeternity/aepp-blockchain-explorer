ec47a0a3508d8e0f56cc0526a1f3175f2a2c4bb8
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-01-11 16:18:53 +0100

    added tx interval page

--- src/screens/transactionList/transactionList.vue
+++ src/screens/transactionList/transactionList.vue
@@ -1,7 +1,45 @@
 <template>
   <div class="tx-list screen">
     <div class="inner">
-      <div class="transactions">
+      <div class="title">
+        Transactions
+      </div>
+      <div class="grid">
+        <AeInput
+          v-model="startGeneration"
+          autofocus
+          type="number"
+          class="startGeneration"
+          name="startGeneration"
+          label="From"
+          placeholder="Start Generation"
+        />
+        <AeLabel>
+          To
+        </AeLabel>
+        <AeInput
+          v-model="endGeneration"
+          autofocus
+          type="number"
+          class="endGeneration"
+          name="endGeneration"
+          label="To"
+          placeholder="End Generation"
+        />
+        <AeButton
+          type="exciting"
+          @click="update"
+        >
+          Submit
+        </AeButton>
+      </div>
+      <AeLoader
+        v-if="!loaded"
+      />
+      <div
+        v-if="loaded"
+        class="transactions"
+      >
         <Transaction
           v-for="t in transactions"
           :key="t.hash"
@@ -12,29 +50,55 @@
   </div>
 </template>
 <script>
+import { AeLoader, AeInput, AeButton, AeLabel } from '@aeternity/aepp-components'
 import Transaction from '../../components/transaction/transaction'
 
 export default {
   name: 'TransactionList',
   components: {
-    Transaction
-  },
-  props: {
+    Transaction,
+    AeLoader,
+    AeInput,
+    AeLabel,
+    AeButton
   },
   data: function () {
     return {
-      transactions: []
+      startGeneration: 0,
+      endGeneration: 0,
+      transactions: [],
+      loaded: false
     }
   },
   beforeMount: async function () {
     const { start, end } = this.$route.params
+    this.startGeneration = start
+    this.endGeneration = end
     try {
-      if (Number(start) > Number(end)) {
-        throw new Error('Invalid start and end generation')
-      }
-      this.transactions = await this.$store.dispatch('transactions/getTxByGeneration', { start, end })
+      this.validate()
+      await this.fetchTx()
     } catch (error) {
       this.$router.push({ path: '/' })
+    }
+  },
+  methods: {
+    update: async function () {
+      this.validate()
+      this.$router.push({ path: `/transactions/${this.startGeneration}/${this.endGeneration}` })
+      await this.fetchTx()
+    },
+    fetchTx: async function () {
+      try {
+        this.loaded = false
+        this.transactions = await this.$store.dispatch('transactions/getTxByGeneration', { start: this.startGeneration, end: this.endGeneration })
+        this.loaded = true
+      } catch (error) {
+      }
+    },
+    validate: function () {
+      if (Number(this.startGeneration) > Number(this.endGenerationd)) {
+        throw new Error('Invalid start and end generation')
+      }
     }
   }
 }
