53fbacb1e0d0d05f94addb0ad80c9bc6e05fb324
artem-rozhkov-dev-optima <artem.rozhkov@dev-optima.com>
2019-01-25 12:11:16 +0200

    add label component

---
+++ src/_designs/components/labelType/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <span
+    class="label-wrapper"
+    :class="{
+      [fill]: Boolean(fill)
+    }"
+  >
+    {{ title }}
+  </span>
+</template>
+<script>
+export default {
+  name: 'LabelType',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    fill: {
+      type: String,
+      validator: value => [
+        'red',
+        'green',
+        'black',
+        'grey'
+      ].includes(value),
+      default: 'grey'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import "~@aeternity/aepp-components-3/src/styles/variables/colors";
+  .label-wrapper {
+    display: table-caption;
+    padding: 5px;
+    text-transform: uppercase;
+    color: white;
+    border-radius: 0 5px 5px 0;
+  }
+  .label-wrapper.red {
+    background-color: red;
+  }
+  .label-wrapper.green {
+    background-color: green;
+  }
+  .label-wrapper.black {
+    background-color: black;
+  }
+  .label-wrapper.grey {
+    background-color: grey;
+  }
+</style>
--- src/_designs/screens/transaction/index.vue
+++ src/_designs/screens/transaction/index.vue
@@ -1,12 +1,76 @@
 <template>
   <div class="app-transaction">
     <h1>App-Transaction</h1>
+    <AppPanel>
+      <AppTable details>
+        <AppTableHeader>
+          <AppTableRow>
+            <AppTableRowColumn width="large">
+              <AppTableCell extend>
+                <div class="app-block-height">
+                  <LabelType
+                    title="Block Height"
+                    fill="black"
+                  />
+                  <span class="app-block-height-num">
+                    6606081
+                  </span>
+                </div>
+                <div class="app-block-confirmations">
+                  <span class="app-block-confirmations-num">
+                    235
+                  </span>
+                  <span class="app-block-confirmations-name">
+                    Block Confirmations
+                  </span>
+                </div>
+              </AppTableCell>
+            </AppTableRowColumn>
+            <AppTableRowColumn width="small">
+              <AppTableCell>
+                <AppDefinition
+                  title="October 11 2018"
+                >
+                  <AeText face="mono-s">
+                    10:59:34 AM +UTC
+                  </AeText>
+                </AppDefinition>
+              </AppTableCell>
+            </AppTableRowColumn>
+          </AppTableRow>
+        </AppTableHeader>
+      </AppTable>
+    </AppPanel>
   </div>
 </template>
 
 <script>
+import AppTable from '@/_designs/components/appTable'
+import AppTableRow from '@/_designs/components/appTableRow'
+import AppTableCell from '@/_designs/components/appTableRowCell'
+import AppTableHeader from '@/_designs/components/appTableHeader'
+import AppTableRowColumn from '@/_designs/components/appTableRowColumn'
+import AppDefinition from '@/_designs/components/appDefinition'
+import AppPanel from '@/_designs/components/appPanel'
+import LabelType from '@/_designs/components/labelType'
+
+import {
+  AeText
+} from '@aeternity/aepp-components-3'
+
 export default {
-  name: 'AppTransaction'
+  name: 'AppTransaction',
+  components: {
+    AppTable,
+    AppTableRow,
+    AppTableCell,
+    AppTableHeader,
+    AppTableRowColumn,
+    AppDefinition,
+    AppPanel,
+    AeText,
+    LabelType
+  }
 }
 </script>
 
