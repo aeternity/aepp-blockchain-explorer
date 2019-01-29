95ba4b74bdbdc20896b55be4eea4e3a5b1b6c435
Emil Sommerfeldt <emil@jungfeldt.de>
2019-01-29 12:30:12 +0100

    add formatting to units

---
+++ src/_designs/components/formatAeUnit/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <span class="format-ae-unit">
+    {{ formattedValue }}
+    <abbr
+      class="unit"
+      title="aeternity"
+    >
+      {{ unit }}
+    </abbr>
+  </span>
+</template>
+<script>
+export default {
+  name: 'FormatAeUnit',
+  props: {
+    value: {
+      type: Number,
+      required: true
+    },
+    type: {
+      type: String,
+      validator: value => [
+        'ae',
+        'fee',
+        'cost'
+      ].includes(value),
+      default: 'ae'
+    }
+  },
+  computed: {
+    formattedValue: function () {
+      if (this.type === 'ae') {
+        return this.value.toLocaleString()
+      }
+      return this.value
+    },
+    unit: function () {
+      if (this.type === 'ae') {
+        return 'AE'
+      }
+      return 'ATTO'
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+
+    .format-ae-unit {
+        & .unit {
+          font-size: .7em;
+          margin-left: -.7em;
+          text-decoration: none;
+        }
+    }
+
+</style>
