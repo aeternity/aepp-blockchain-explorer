d583bb08f539da9b222930172b5054142eba6463
emilsomm <emil@jungfeldt.de>
2019-01-31 10:38:01 +0100

    update responsiveness for app-table components

--- src/_designs/components/appTableRow/index.vue
+++ src/_designs/components/appTableRow/index.vue
@@ -39,7 +39,7 @@ export default {
     padding-top: .6rem;
     margin-bottom: 0;
     border-left: none;
-    @media (min-width: 450px) {
+    @media (min-width: 550px) {
       width: 50%;
       padding-top: 0;
       margin-top: 0;
@@ -56,8 +56,11 @@ export default {
     &:not(:first-child) {
       border-left:  2px solid $color-neutral-positive-2;
     }
-    @media (min-width: 450px) {
+    @media (min-width: 550px) {
       width: 50%;
     }
+  }
+  .app-table-row > .app-table-row.extend {
+    border-top: 2px solid $color-neutral-positive-2;
   }
 </style>
