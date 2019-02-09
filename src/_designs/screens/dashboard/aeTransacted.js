2ef41c83ea01ccd1e75ccd276cd9babb80103e85
ror-shubham <ror.shubh@gmail.com>
2019-02-09 20:50:14 +0530

    added graphs on dashboard with fake data

---
+++ src/_designs/screens/dashboard/aeTransacted.js
@@ -0,0 +1,58 @@
+
+export const aeTransacted = {
+  type: 'line',
+  data: {
+    labels: ['1/1/19', '2/1/19', '3/1/19', '4/1/19', '5/1/19', '6/1/19', '7/1/19', '8/1/19', '9/1/19'],
+    datasets: [
+      {
+        label: 'Transactions',
+        data: [1110, 1111110, 1, 11111, 2111, 6711111, 6111112, 27111111, 14],
+        fill: false,
+        borderColor: [
+          '#FF0D6A'
+        ],
+        borderWidth: 3
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    lineTension: 1,
+    legend: {
+      display: false
+    },
+    title: {
+      display: true,
+      text: 'AE transacted per day',
+      fontSize: 18
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            padding: 25,
+            callback: function (label, index, labels) {
+              return label / 1000 + 'AE'
+            },
+            fontSize: 12
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '1k = 1000',
+            fontSize: 12
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 12
+          }
+        }
+      ]
+    }
+  }
+}
+
+export default aeTransacted
--- src/_designs/screens/dashboard/index.vue
+++ src/_designs/screens/dashboard/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="app-dashboard">
-    <PageHeader title="Dashboard">
-      <BreadCrumbs />
-    </PageHeader>
+    <div class="graphs">
+      <div class="graph-transaction-rate">
+        <canvas id="transaction-rate" />
+      </div>
+      <div class="graph-ae-transacted">
+        <canvas id="ae-transacted" />
+      </div>
+    </div>
     <Generations v-if="generations">
       <Generation
         v-for="(generation, number) in generations"
@@ -15,27 +20,61 @@
 <script>
 import Generations from '@/_designs/components/generations'
 import Generation from '@/_designs/components/generation'
-import PageHeader from '@/_designs/components/PageHeader'
-import BreadCrumbs from '@/_designs/components/BreadCrumbs'
 import { mapState } from 'vuex'
+import Chart from 'chart.js'
+import transactionRate from './transactionRate'
+import aeTransacted from './aeTransacted'
 
 export default {
   name: 'AppDashboard',
   components: {
-    PageHeader,
     Generations,
-    Generation,
-    BreadCrumbs
+    Generation
   },
   computed:
     mapState('blocks', [
       'generations'
     ]),
+  data () {
+    return {
+      transactionRate: transactionRate,
+      aeTransacted: aeTransacted
+    }
+  },
   mounted: async function () {
+    this.createChart('transaction-rate', this.transactionRate)
+    this.createChart('ae-transacted', this.aeTransacted)
     await this.$store.dispatch('blocks/getLatestGenerations', 5)
+  },
+  methods: {
+    createChart (chartId, chartData) {
+      const ctx = document.getElementById(chartId)
+      new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      })
+    }
   }
 }
 </script>
 <style lang="scss">
-.app-dashboard {}
+.app-dashboard {
+  .graphs{
+    canvas{
+      width:100%;
+      height: auto;
+    }
+    .graph-transaction-rate{
+      max-width:40em;
+      max-height: 20em;
+      margin-bottom: 10em;
+    }
+    .graph-ae-transacted{
+      margin-bottom: 10em;
+      max-width:40em;
+      max-height: 20em;
+    }
+  }
+}
 </style>
