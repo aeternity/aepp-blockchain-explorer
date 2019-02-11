7c9c4bbecd830abe9352bac95bbf6805b5ab24d6
artem-rozhkov-dev-optima <artem.rozhkov@dev-optima.com>
2019-02-11 16:19:19 +0200

    update tx list with headers

--- src/_designs/screens/dashboard/index.vue
+++ src/_designs/screens/dashboard/index.vue
@@ -3,6 +3,7 @@
     <PageHeader title="Dashboard">
       <BreadCrumbs />
     </PageHeader>
+    <h1>Generations</h1>
     <Generations v-if="generations">
       <Generation
         v-for="(generation, number) in generations"
@@ -10,11 +11,22 @@
         :data="generation"
       />
     </Generations>
+    <br>
+    <h1>Transactions</h1>
+    <TxList v-if="transactionsList.length">
+      <TXListItem
+        v-for="(transaction, index) in transactionsList"
+        :key="index"
+        :data="transaction.tx"
+      />
+    </TxList>
   </div>
 </template>
 <script>
 import Generations from '@/_designs/components/generations'
 import Generation from '@/_designs/components/generation'
+import TxList from '@/_designs/components/transactions/txList'
+import TXListItem from '@/_designs/components/transactions/txListItem'
 import PageHeader from '@/_designs/components/PageHeader'
 import BreadCrumbs from '@/_designs/components/BreadCrumbs'
 import { mapState } from 'vuex'
@@ -25,17 +37,38 @@ export default {
     PageHeader,
     Generations,
     Generation,
-    BreadCrumbs
+    BreadCrumbs,
+    TxList,
+    TXListItem
+  },
+  data () {
+    return {
+      transactionsList: []
+    }
   },
   computed:
     mapState('blocks', [
-      'generations'
+      'generations',
+      'height'
     ]),
   mounted: async function () {
     await this.$store.dispatch('blocks/getLatestGenerations', 5)
+    await this.$store.dispatch('blocks/height')
+    await this.fetchTx()
+  },
+  methods: {
+    fetchTx: async function () {
+      try {
+        this.transactionsList = await this.$store.dispatch('transactions/getTxByGeneration', {
+          start: this.height - 10,
+          end: this.height
+        })
+      } catch (error) {
+      }
+    }
   }
 }
 </script>
 <style lang="scss">
-.app-dashboard {}
+  .app-dashboard {}
 </style>
