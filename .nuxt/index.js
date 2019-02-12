3f41b290da48fff42222027aae7d1ac4589f6926
Emil Sommerfeldt <emil@jungfeldt.de>
2019-02-12 16:10:12 +0100

    add .nuxt to gitignore

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -10,8 +10,6 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_axios_1114b149 from 'nuxt_plugin_axios_1114b149' // Source: ./axios.js (mode: 'all')
-
 // Component: <NoSsr>
 Vue.component(NoSsr.name, NoSsr)
 
@@ -103,32 +101,7 @@ async function createApp(ssrContext) {
     beforeRenderFns: ssrContext ? ssrContext.beforeRenderFns : undefined
   })
 
-  const inject = function (key, value) {
-    if (!key) throw new Error('inject(key, value) has no key provided')
-    if (typeof value === 'undefined') throw new Error('inject(key, value) has no value provided')
-    key = '$' + key
-    // Add into app
-    app[key] = value
-
-    // Check if plugin not already installed
-    const installKey = '__nuxt_' + key + '_installed__'
-    if (Vue[installKey]) return
-    Vue[installKey] = true
-    // Call Vue.use() to install the plugin into vm
-    Vue.use(() => {
-      if (!Vue.prototype.hasOwnProperty(key)) {
-        Object.defineProperty(Vue.prototype, key, {
-          get() {
-            return this.$root.$options[key]
-          }
-        })
-      }
-    })
-  }
-
   // Plugin execution
-
-  if (typeof nuxt_plugin_axios_1114b149 === 'function') await nuxt_plugin_axios_1114b149(app.context, inject)
 
   // If server-side, wait for async component to be resolved first
   if (process.server && ssrContext && ssrContext.url) {
