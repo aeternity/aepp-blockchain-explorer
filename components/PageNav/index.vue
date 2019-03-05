e53ed17675aa102f3daf5f75954d5fecd8d0a46c
emilsomm <emil@jungfeldt.de>
2019-03-05 14:10:53 +0100

    add dynamic breadcrumbs and pagenav

--- components/PageNav/index.vue
+++ components/PageNav/index.vue
@@ -3,17 +3,42 @@
     class="page-nav"
   >
     <ul class="page-nav-list">
-      <li class="page-nav-list-item prev">
-        <span class="visually-hidden">
-          previous page
+      <li
+        class="page-nav-list-item prev"
+      >
+        <nuxt-link
+          v-if="prev"
+          :to="prev"
+        >
+          <span class="visually-hidden">
+            previous page
+          </span>
+          <AppIcon name="back" />
+        </nuxt-link>
+        <span
+          v-else
+          class="disabled"
+        >
+          <AppIcon name="back" />
         </span>
-        <AppIcon name="back" />
       </li>
       <li class="page-nav-list-item next">
-        <span class="visually-hidden">
-          next page
+        <nuxt-link
+          v-if="next"
+          :to="next"
+          @click="goTo"
+        >
+          <span class="visually-hidden">
+            next page
+          </span>
+          <AppIcon name="back" />
+        </nuxt-link>
+        <span
+          v-else
+          class="disabled"
+        >
+          <AppIcon name="back" />
         </span>
-        <AppIcon name="back" />
       </li>
     </ul>
   </nav>
@@ -25,6 +50,21 @@ export default {
   name: 'PageNav',
   components: {
     AppIcon
+  },
+  props: {
+    prev: {
+      type: String,
+      default: ''
+    },
+    next: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    goTo () {
+      this.$emit('get')
+    }
   }
 }
 </script>
@@ -44,14 +84,16 @@ export default {
         display: flex;
         align-items: center;
         font-size: 1.3rem;
-        & .ae-icon {
-          color: $color-neutral-negative-3;
-        }
+        text-transform: capitalize;
         &.prev {
           margin-right: 1rem;
         }
-        &.next > .app-icon {
+        &.next .app-icon {
           transform: rotate(180deg);
+        }
+        & .disabled {
+          color: $color-neutral-negative-1;
+          cursor: not-allowed;
         }
       }
     }
